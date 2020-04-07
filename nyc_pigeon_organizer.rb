dffe21b5093f495906c62d7b6c2ec85d0f228712
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-07 06:55:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  new_hash = {}
   
-  data.reduce(new_hash) do |memo, (outer_key,attr_hash)|
-    attr_hash.each do |key,value|
-        value.each do |name|
-          if memo[name]
-            if memo[name][key] 
-              memo[name][key] << value
-            end
-          end
-        end
-    end
-  end
 end
+
+
+
+
+
+
+
+# data.reduce, iterate over three keys in data
+# each key (:color) is a hash, so we must iterate over each key 
+# key.each, iterate over each key hash - key and array 
+
+# iterate over arrays, check if name in array is a key in the new_hash
+# if it exists we can add 
