c9a70a76735a4c2cd799c22882a83a81d3b546e6
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-07 07:57:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each do |outer_key, attr_hash|
+
+    attr_hash.each do |attr_key, attr_arr|
+
+      attr_arr.each do |name|
+        if new_hash[name] 
+          new_hash[name][outer_key] << attr_key.to_s
+        else
+          new_hash[name] = {
+            :color => [],
+            :gender => [],
+            :lives => []
+          }
+          new_hash[name][outer_key] << attr_key.to_s
+        end
+      end
+    end
+
+  end
+  new_hash
 end
