147d88ec4879143440339b4c32b9ba8182b25d29
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-07 06:39:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,9 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   
-  data.reduce(new_hash) do |memo, (key,value)|
-    
-  
+  data.reduce(new_hash) do |memo, (key,attr_hash)|
+    puts memo
+    puts key
+    puts attr_hash
   end
 end
