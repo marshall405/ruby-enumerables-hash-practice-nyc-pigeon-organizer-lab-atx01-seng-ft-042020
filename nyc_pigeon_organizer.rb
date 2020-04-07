9e74dcbf4f3aa14a457d69ceb2dd39effe653f50
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-07 07:44:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,22 +22,3 @@ def nyc_pigeon_organizer(data)
   end
   new_hash
 end
-
-
-
-
-
-
-
-# data.reduce, iterate over three keys in data
-# each key (:color) is a hash, so we must iterate over each key 
-# key.each, iterate over each key hash - key and array 
-
-# iterate over arrays, check if name in array is a key in the new_hash
-# if it exists we can add element to new_hash[name][attr] => which is an array
-
-#if it doesnt exist we must create it. new_hash[name] = {
-#   color => []
-#   gender => []
-#   lives => []
-# }
