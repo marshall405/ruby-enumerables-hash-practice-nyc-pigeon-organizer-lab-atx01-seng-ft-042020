a32ca139e4c48d6e90a61aa9e3c7764c0546d73f
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-07 07:12:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
     
     attr_hash.each do |attr_key, attr_arr|
       attr_arr.each do |name|
-        puts memo 
+        puts name
         puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         if memo[name] 
           memo[name][outer_key] << attr_key
